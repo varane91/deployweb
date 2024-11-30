7712f2c46d5121feda3146c7b681010394ed4cd6
varane91 <165835834+varane91@users.noreply.github.com>
2024-11-30 17:33:55 -0500

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('contactButton').addEventListener('click', function() {
-    alert('¡Gracias por tu interés! Puedes encontrarnos en la Facultad de Ciencias de la Educación');
+    alert('¡Gracias por tu interés! Puedes encontrarnos en la Facultad de Ciencias de la Educación. ISPJAM. UO.');
 });
 
